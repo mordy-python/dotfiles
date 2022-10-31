0e1e14f0d810c5be426a3d0a40844bcad1410c2d
Israel Waldner <60708054+mordy-python@users.noreply.github.com>
2022-10-31 17:59:01 +0000

    Added setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
-sudo unzip JetBrainsMono-2.242.zip -d /usr/share/fonts
-sudo fc-cache -fc
+if [[ ! -f "JetBrainsMono-2.242.zip" ]]; then
+    wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
+fi
+# else
+    sudo unzip JetBrainsMono-2.242.zip -d /usr/share/fonts
+    sudo mkdir /root/.cache/fontconfig
+    sudo mkdir /root/.fontconfig
+    sudo fc-cache -frv
+alias cls="clear"
